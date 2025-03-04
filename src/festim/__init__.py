6d16f0f9c8dcfc68832930b40152edbb2ac6f37e
jhdark <darkj385@mit.edu>
2025-03-04 13:58:14 -0500

    add to festim init

--- src/festim/__init__.py
+++ src/festim/__init__.py
@@ -16,6 +16,7 @@ from .boundary_conditions.dirichlet_bc import (
     FixedConcentrationBC,
     FixedTemperatureBC,
 )
+from .boundary_conditions.implantation_dirichlet import ImplantationDirichlet
 from .boundary_conditions.flux_bc import FluxBCBase, HeatFluxBC, ParticleFluxBC
 from .boundary_conditions.henrys_bc import HenrysBC
 from .boundary_conditions.sieverts_bc import SievertsBC
--- src/festim/boundary_conditions/__init__.py
+++ src/festim/boundary_conditions/__init__.py
@@ -1,11 +1,13 @@
 from .dirichlet_bc import FixedConcentrationBC, FixedTemperatureBC
 from .flux_bc import HeatFluxBC, ParticleFluxBC
+from .implantation_dirichlet import ImplantationDirichlet
 from .surface_reaction import SurfaceReactionBC
 
 __all__ = [
     "FixedConcentrationBC",
     "FixedTemperatureBC",
+    "HeatFluxBC",
+    "ImplantationDirichlet",
     "ParticleFluxBC",
     "SurfaceReactionBC",
-    "HeatFluxBC",
 ]
